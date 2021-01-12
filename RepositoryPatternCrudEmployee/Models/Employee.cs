1862d0ac26e3aa4690672efbebce031cb55ec5aa
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-01-12 12:39:22 -0400

    ApplicationDbContext, Services and ConnectionStrings json added

--- RepositoryPatternCrudEmployee/Models/Employee.cs
+++ RepositoryPatternCrudEmployee/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,25 @@ namespace RepositoryPatternCrudEmployee.Models
 {
     public class Employee
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es requerido")]
+        [Display(Name ="Nombre")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "La dirrecion es requerida")]
+        [Display(Name = "Direccion")]
+        public string Address { get; set; }
+
+
+        [Required(ErrorMessage = "El salario es requerido")]
+        [Display(Name = "Salario")]
+        public int Salary { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name="Correo")]
+        public string Email { get; set; }
+
     }
 }
